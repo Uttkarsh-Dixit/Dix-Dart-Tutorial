0f2791ee0d4aa00916ba6f9d2c6fcb5286cc9e18
Uttkarshdixit <dixituttkarsh5@gmail.com>
2025-08-20 15:56:04 +0530

    Fibonacci Series up to "nth" times.

---
+++ passwordGenerator.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  stdout.write("Enter size of password: ");
+  String? value = stdin.readLineSync();
+
+  if(value != null && value.isNotEmpty) {
+    try {
+      int size = int.parse(value);
+      stdout.write("Password: ");
+      print(generatePassword(size));
+    }
+        catch(e) {
+          print("Enter a valid size! ");
+        }
+  }
+  else {
+    print("No size provided! ");
+  }
+}
+
+String generatePassword (int size) {
+  final String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  final String lower = "abcdefghijklmopqrstuvwxyz";
+  final String digits = '1234567890';
+  final String chars = r'!@#$%^&*()~_+-=<>?:"{}|[]\;,';
+
+  final String all = upper + lower + digits + chars;
+
+  final Random rnd = Random.secure();
+
+  return List.generate(
+      size,
+      (_)=>all[rnd.nextInt(all.length)]
+  ).join();
+}
